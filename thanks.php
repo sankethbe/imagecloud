66dcc4968828945b7126153509dbebf708a00fc8
sankethbe <sankethbe@gmail.com>
2014-01-23 12:50:42 -0500

    Create thanks.php

---
+++ thanks.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Image Magick in the Cloud - Image Uploaded</title>
+
+<style>
+div#container {
+	width: 80%;
+	margin:0 auto;
+	text-align:center;
+}
+</style>
+</head>
+
+<body>
+<div id="container">
+<img src="images/main.jpg" width="300" height="217" alt="ImageMagick">
+
+<h1>Thanks!</h1>
+<p>You will receive an email soon with confirmation information.</p> 
+<p>Shortly after that, you will receive another email when your file has been processed.</p>
+
+<a href='http://ec2-54-204-212-149.compute-1.amazonaws.com/process.php'>Click here to process the image.</a>
+
+</div>
+</body>
+</html>
